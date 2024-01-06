a616a41a33876c563494729904fbf6d5091bffd9
rich <rich.leblanc1@proton.me>
2024-01-06 12:57:54 -0500

    01_variables

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # from_python_to_rust
+
+to run:
+cd /home/rich/RustroverProjects/from_python_to_rust/src
+rustc main.rs
+./main 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 1;
+    println!("x: {}", x);
+    // binds x again, shadowing the old one from above
+    let x = 'i';
+    println!("x: {}", x);
+
+    // declare, initialize
+    let something;
+    let x = 5;
+    something = x * 5;
+    println!("x, something: {}, {}", x, something);
+
+    // // Mutability
+    let mut y = 0;
+    y = y * 2 + x;
+    dbg!(y);
+
+    const BLAH: i32 = 42;
+    y *= BLAH;
+    dbg!(y);
 }
