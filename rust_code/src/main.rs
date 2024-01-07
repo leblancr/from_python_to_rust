92dae12ce6d1f5f1bb1d40cd4e3478f5cc8d260c
rich leblanc <rich.leblanc1@proton.me>
2024-01-07 15:08:35 -0500

    01_numbers - finished scope

--- rust_code/src/main.rs
+++ rust_code/src/main.rs
@@ -1,24 +1,30 @@
 fn main() {
 
-    let x = 1;
-    println!("x: {}", x);
-    // binds x again, shadowing the old one from above
-    let x = 'i';
-    println!("x: {}", x);
+    // scope
+    // This binding lives in the main function
+    let long_lived_binding = 1;
 
-    // declare, initialize
-    let something;
-    let x = 5;
-    //println!("x, something: {}, {}", x, something);  // uninitialized
-    something = x * 5;
-    println!("x, something: {}, {}", x, something);
+    // This is a block, and has a smaller scope than the main function
+    {
+        // This binding only exists in this block
+        let short_lived_binding = 2;
 
-    // Mutability
-    let mut y = 0;
-    y = y * 2 + x;
-    dbg!(y);
+        println!("inner short: {}", short_lived_binding);
 
-    const BLAH: i32 = 42;
-    y *= BLAH;
-    dbg!(y);
+        // This binding *shadows* the outer one
+        let long_lived_binding = 5_f32;
+
+        println!("inner long: {}", long_lived_binding);
+    }
+    // End of the block
+
+    // Error! `short_lived_binding` doesn't exist in this scope
+    //println!("outer short: {}", short_lived_binding);
+
+    println!("outer long: {}", long_lived_binding);
+
+    // This binding also *shadows* the previous binding
+    let long_lived_binding = 'a';
+
+    println!("outer long: {}", long_lived_binding);
 }
