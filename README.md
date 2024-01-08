b7277105a0b43151ec3a634ab126867311f40d37
rich leblanc <rich.leblanc1@proton.me>
2024-01-07 22:39:51 -0500

    01_numbers - finished numbers

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ poetry shell
 Rust:
 In main root project folder run:
 cargo new rust_code
+cargo add num - Adding num v0.4.1 to dependencies.
 
 To run:
 cd rust_code
--- rust_code/src/main.rs
+++ rust_code/src/main.rs
@@ -1,30 +1,48 @@
 fn main() {
+    // integers
+    let x: i32 = -1000;
+    println!("x: {}", x);
+    let x = -1000i32;
+    let y: u64 = 100;
+    println!("y: {}", y);
+    let y = 100u64;
+    let large_number = 100_000_000;
 
-    // scope
-    // This binding lives in the main function
-    let long_lived_binding = 1;
+    println!("x: {}", x);
+    println!("y: {}", y);
+    println!("large_number: {}", large_number);
 
-    // This is a block, and has a smaller scope than the main function
-    {
-        // This binding only exists in this block
-        let short_lived_binding = 2;
+    dbg!(x, y);
 
-        println!("inner short: {}", short_lived_binding);
+    // floats
+    let xf: f32 = -1.2345;
+    println!("xf: {}", xf);
+    let xf = -1.2345_f32;
+    println!("xf: {}", xf);
 
-        // This binding *shadows* the outer one
-        let long_lived_binding = 5_f32;
+    // complex numbers
+    // not part of rusts standard library
+    let complex_integer = num::complex::Complex::new(10, 20);
+    let complex_float = num::complex::Complex::new(10.1, 20.1);
+    println!("complex_integer: {}", complex_integer);
+    println!("complex_float: {}", complex_float);
 
-        println!("inner long: {}", long_lived_binding);
-    }
-    // End of the block
 
-    // Error! `short_lived_binding` doesn't exist in this scope
-    //println!("outer short: {}", short_lived_binding);
+    // binary literals
+    let x: u8 = 0b1010_1010;
+    let y: u8 = 0b0101_0101;
+    dbg!(x | y);
 
-    println!("outer long: {}", long_lived_binding);
+    // hex
+    let x: u8 = 0xAF;
+    dbg!(x);
+    let largex = 0xABCD_EF01_u32;
+    dbg!(largex);
 
-    // This binding also *shadows* the previous binding
-    let long_lived_binding = 'a';
+    // boolean
+    let yes = true;
+    let no = false;
 
-    println!("outer long: {}", long_lived_binding);
+    dbg!(yes);
+    dbg!(no);
 }
